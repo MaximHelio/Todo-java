92ee2497a756a2b5f9cd4453c845d564fecc7417
kknni <gasiorowicz10@gmail.com>
2022-09-11 22:45:09 +0900

    first todo

---
+++ src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,28 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // title이 없으면 db에 넣는 게 의미가 없기 때문에 nullable=false
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "todoOrder", nullable = false)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean completed;
+}
---
+++ src/test/java/org/example/service/TodoServiceTest.java
@@ -0,0 +1,73 @@
+package org.example.service;
+
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.repository.TodoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    // Mock 사용하는 이유: 외부 시스템에 의존하지 않고 자체 시스템에서 구성할 수 있어야 하기 때문
+    // 또한, 실제 DB에 값이 함부로 추가되거나 삭제되는 일을 막기 위해 
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void add() {
+        when(this.todoRepository.save(any(TodoEntity.class)))
+                .then(AdditionalAnswers.returnsFirstArg());
+
+        TodoRequest expected = new TodoRequest();
+        expected.setTitle("Test Title");
+
+        TodoEntity actual = this.todoService.add(expected);
+        assertEquals(expected.getTitle(), actual.getTitle());
+    }
+
+    @Test
+    void searchById() {
+        TodoEntity entity = new TodoEntity();
+        entity.setId(123L);
+        entity.setTitle("TITLE");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+        TodoEntity expected = optional.get();
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getOrder(), actual.getOrder());
+        assertEquals(expected.getCompleted(), actual.getCompleted());
+    }
+
+    @Test
+    public void searchByIdFailed(){
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+        assertThrows(ResponseStatusException.class, ()-> {
+            this.todoService.searchById(123L);
+        });
+    }
+}
